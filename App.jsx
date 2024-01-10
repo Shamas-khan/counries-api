84e8bac1b86353b91e90e37f9fe0754f1d8a9acf
shmas khan <sktanoli420@gmail.com>
2024-01-11 03:18:34 +0500

    Implement Dark Mode functionality

--- App.jsx
+++ App.jsx
@@ -1,16 +1,14 @@
-import React, { useState } from "react";
 import Header from "./Components/Header";
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import { ThemeContext } from "./contexts/ThemeContext";
+import { ThemeProvider } from "./contexts/ThemeContext";
 
 const App = () => {
-  const[isDark, isNotDark] = useState(JSON.parse(localStorage.getItem('DarkMode')))
   return (
-    <ThemeContext.Provider value={[isDark, isNotDark]}>
-      <Header/>
-      <Outlet/>
-    </ThemeContext.Provider>
+    <ThemeProvider>
+      <Header />
+      <Outlet />
+    </ThemeProvider>
   );
 };
 
--- contexts/ThemeContext.jsx
+++ contexts/ThemeContext.jsx
@@ -1,3 +1,13 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const ThemeContext = createContext()
+
+export function ThemeProvider({children}){
+    const[isDark, isNotDark] = useState(JSON.parse(localStorage.getItem('DarkMode')))
+
+    return(
+        <ThemeContext.Provider value={[isDark, isNotDark]}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
